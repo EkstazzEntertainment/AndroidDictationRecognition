8b18ab602fec9b56cc9b5597da29ae0361e5f544
alexanian <alexanyanyuka@gmail.com>
2023-07-06 12:29:41 +0200

    Base commit.

--- unityplugin/src/main/java/com/ekstazz/unityplugin/MainScript.java
+++ unityplugin/src/main/java/com/ekstazz/unityplugin/MainScript.java
@@ -1,29 +1,26 @@
 package com.ekstazz.unityplugin;
 
-import static android.speech.SpeechRecognizer.ERROR_AUDIO;
-import static android.speech.SpeechRecognizer.ERROR_CLIENT;
-import static android.speech.SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS;
-import static android.speech.SpeechRecognizer.ERROR_NETWORK;
-import static android.speech.SpeechRecognizer.ERROR_NETWORK_TIMEOUT;
-import static android.speech.SpeechRecognizer.ERROR_RECOGNIZER_BUSY;
-import static android.speech.SpeechRecognizer.ERROR_SERVER;
-import static android.speech.SpeechRecognizer.ERROR_SPEECH_TIMEOUT;
-
+import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
+import android.app.Fragment;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
-import android.speech.RecognitionService;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import java.util.ArrayList;
-import android.app.Service;
-import static android.speech.SpeechRecognizer.createSpeechRecognizer;
+import java.util.IllformedLocaleException;
+import java.util.Locale;
 
 
-public class MainScript extends RecognitionService implements RecognitionListener
+public class MainScript extends Fragment
 {
     private static final int REQUEST_SPEECH = 1;
 
@@ -46,292 +43,260 @@ public class MainScript extends RecognitionService implements RecognitionListene
 
     public void StartSpeechRecognition()
     {
-        //System.out.println("13456709876543ewrtyuiopuytre--------------");
-        StartListening();
-    }
-
-    public static SpeechRecognizer m_EngineSR;
-    public static Context mContext;
-    public static MainScript mSpeechService;
-
-    static String TAG = "VOICE RECOGNITION";
-
-
-    public void StartListening()
-    {
-        System.out.println("-22----33----44----- " + "START THE SERVICE!");
-        Intent intent = new Intent(unityActivity, MainScript.class);
-        unityActivity.startService(intent);
-
-        onCreate();
+        System.out.println("->->->->->->->->->--------------");
+        RequestAudioPermission();
     }
 
 
-    @Override
-    public void onCreate()
-    {
-        System.out.println("-22----33----44----- " + "onCreate()");
 
-        if(mSpeechService == null)
+
+
+
+
+
+
+
+
+
+
+    private static final String TAG = "SpeechRecognizer";
+    // Singleton instance.
+    public static MainScript instance;
+
+    //UNITY CONTEXT
+    String gameObjectName;
+    String resultCallbackName = "OnResult";
+    String errorCallbackName = "OnError";
+
+    private static String finalResult = "";
+
+
+    public static void SendUnityResults(String results)
+    {
+        //UnityPlayer.UnitySendMessage(instance.gameObjectName, instance.resultCallbackName, results);
+        Log.d(TAG, results);
+
+        finalResult = results;
+    }
+
+    public String GetResultString()
+    {
+        return finalResult;
+    }
+
+    public static void SendUnityError(ERROR error)
+    {
+        String errorString = String.valueOf(error);
+        //UnityPlayer.UnitySendMessage(instance.gameObjectName, instance.errorCallbackName, errorString);
+        Log.d(TAG, errorString);
+    }
+
+    public static void SetUp(String gameObjectName)
+    {
+        instance = new MainScript();
+        instance.gameObjectName = gameObjectName; // Store 'GameObject' reference
+        //UnityPlayer.currentActivity.getFragmentManager().beginTransaction().add(instance, SpeechRecognizerFragment.TAG).commit();
+    }
+
+    public void RequestAudioPermission()
+    {
+        RequestRecordAudioPermission();
+    }
+
+    //SPEECH RECOGNIZER
+    public SpeechRecognizer sr;
+    public SpeechRecognitionListener speechListener = new SpeechRecognitionListener();
+    private static boolean isLanguageSpecified = false;
+    private static String language = "en-US";
+    private static int maxResults = 10;
+
+    public enum ERROR
+    {
+        UNKNOWN, INVALID_LANGUAGE_FORMAT
+    }
+
+
+    private void RequestRecordAudioPermission()
+    {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(unityActivity, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
         {
-            System.out.println("-22----33----44----- " + "start listening");
-
-            mContext = unityActivity.getApplicationContext(); //getApplicationContext();
-            mSpeechService = this;
-            m_EngineSR = createSpeechRecognizer(this);
-            m_EngineSR.setRecognitionListener(this);
-            Intent voiceIntent = RecognizerIntent.getVoiceDetailsIntent(mContext);
-            m_EngineSR.startListening(voiceIntent);
-        }
-        super.onCreate();
-    }
-
-    private void checkForCommands(Bundle bundle)
-    {
-        System.out.println("-22----33----44----- " + "check for commands");
-
-        ArrayList<String> voiceText = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-        if (voiceText != null)
-        {
-            System.out.println("-22----33----44----- " + "voice text not null");
-
-            if (voiceText.size() > 0)
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(unityActivity, Manifest.permission.RECORD_AUDIO))
             {
-                System.out.println("-22----33----44----- " + voiceText.get(0));
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
             }
             else
             {
-                System.out.println("-22----33----44----- " + "nothing found");
-
-                //SendMessageToUnity("nothing!");
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(unityActivity, new String[]{Manifest.permission.RECORD_AUDIO},20);
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
             }
-
         }
         else
         {
-            System.out.println("-22----33----44----- " + "voice text empty");
-
-            //SendMessageToUnity("voiceText empty!");
+            // Permission has already been granted
+            Log.d(TAG, "Permission has already been granted");
         }
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId)
+    public void StartListening()
     {
-        System.out.println("-22----33----44----- " + "onStartCommand");
+        unityActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "StartListeningCalled");
+                sr = SpeechRecognizer.createSpeechRecognizer(unityActivity);
+                sr.setRecognitionListener(speechListener);
 
-        //SendMessageToUnity("onStartCommand()!");
-        if(mSpeechService != null)
-        {
-            System.out.println("-22----33----44----- " + "restarting");
-
-            mContext = getApplicationContext();
-            restartListening(mContext);
-        }
-
-        return Service.START_NOT_STICKY;
-    }
-
-    public static void restartListening(Context context_)
-    {
-        if(mSpeechService != null)
-        {
-            System.out.println("-22----33----44----- " + "restarting");
-
-            mContext = context_;
-            if (m_EngineSR == null)
-            {
-                m_EngineSR = createSpeechRecognizer(mSpeechService);
-                m_EngineSR.setRecognitionListener(mSpeechService);
-            }
-            else
-            {
-                m_EngineSR.stopListening();
-            }
-            Intent voiceIntent = RecognizerIntent.getVoiceDetailsIntent(mContext);
-            m_EngineSR.startListening(voiceIntent);
-        }
-    }
-
-    //public void SendMessageToUnity(String text)
-    //{
-        /*if(m_EngineSR != null)
-        {
-            if(text != null && text.isEmpty())Log.i("TESTING: ", "final message! =" + text);
-            try
-            {
-                if(UnityPlayer.currentActivity != null)
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                if (isLanguageSpecified)
                 {
-                    UnityPlayer.UnitySendMessage("CubeMessage", "GetMessage", text);
-                    SendStopWritingUnity();
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
+                }
+                else
+                {
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                }
+                intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, maxResults);
+
+                try
+                {
+                    sr.startListening(intent);
+                }
+                catch (ActivityNotFoundException a)
+                {
+                    Log.d(TAG, a.toString());
                 }
             }
-            catch (Exception e)
-            {
-                // Log.e(TAG, "UnitySendMessage failed" + e.getMessage());
-            }
-        }*/
-    //}
-
-    public void SendErrorToUnity(String text)
-    {
-        if(m_EngineSR != null)
-        {
-            try
-            {
-
-            }
-            catch (Exception e)
-            {
-
-            }
-        }
+        });
     }
 
-    public void SendStopWritingUnity()
+    public void StartListening(boolean isContinuous, String language, int maxResults)
     {
-        if(m_EngineSR != null)
-        {
-            try
-            {
+        SetContinuousListening(isContinuous);
+        SetLanguage(language);
+        SetMaxResults(maxResults);
+        StartListening();
 
-            }
-            catch (Exception e)
-            {
-
-            }
-        }
+        finalResult = "";
     }
 
-    @Override
-    public void onDestroy()
+    public void StopListening()
     {
-        if(m_EngineSR!= null)
-        {
-            m_EngineSR.cancel();
-        }
-
-        System.out.println("-22----33----44----- " + "service stopped");
-        Log.i("SimpleVoiceService", "Service stopped");
-        super.onDestroy();
+        sr.destroy();
+        sr = null;
     }
 
-    @Override
-    protected void onStartListening(Intent recognizerIntent, Callback listener)
+    public void RestartListening()
     {
-        System.out.println("-22----33----44----- " + "on start listening");
+        StopListening();
+        StartListening();
     }
 
-    @Override
-    protected void onCancel(Callback listener)
+    public void SetContinuousListening(boolean isContinuous)
     {
-        System.out.println("-22----33----44----- " + "onCancel");
+        speechListener.continuousListening = isContinuous;
     }
 
-    @Override
-    protected void onStopListening(Callback listener)
-    {
-        System.out.println("-22----33----44----- " + "onStopListening");
-        SendStopWritingUnity();
-    }
-
-
-    @Override
-    public void onReadyForSpeech(Bundle params)
-    {
-        System.out.println("-22----33----44----- " + "onReadyForSpeech");
-    }
-
-    @Override
-    public void onBeginningOfSpeech()
-    {
-        System.out.println("-22----33----44----- " + "onBeginningOfSpeech");
-    }
-
-    @Override
-    public void onRmsChanged(float rmsdB)
-    {
-
-    }
-
-    @Override
-    public void onBufferReceived(byte[] buffer)
-    {
-
-    }
-
-    @Override
-    public void onEndOfSpeech()
-    {
-        System.out.println("-22----33----44----- " + "onEndOfSpeech");
-    }
-
-    @Override
-    public void onError(int error)
+    public void SetLanguage(String newLanguage)
     {
         try
         {
-            String message;
-            switch (error)
+            Locale locale = new Locale.Builder().setLanguageTag(newLanguage).build();
+            language = newLanguage;
+            isLanguageSpecified = true;
+        }
+        catch (IllformedLocaleException exception)
+        {
+            isLanguageSpecified = false;
+            SendUnityError(ERROR.INVALID_LANGUAGE_FORMAT);
+        }
+    }
+
+
+    public void SetMaxResults(int newMaxResults)
+    {
+        maxResults = newMaxResults;
+    }
+
+
+    //SPEECH RECOGNIZER_LISTENER
+    class SpeechRecognitionListener implements RecognitionListener
+    {
+        private ArrayList<String> resultData = new ArrayList<>();
+        public boolean continuousListening = false;
+
+
+        public void onReadyForSpeech(Bundle params)
+        {
+            Log.d(TAG, "onReadyForSpeech");
+        }
+
+        public void onBeginningOfSpeech()
+        {
+            Log.d(TAG, "onBeginningOfSpeech");
+        }
+
+        public void onRmsChanged(float rmsdB)
+        {
+            /*Log.d(TAG, "onRmsChanged");*/
+        }
+
+        public void onBufferReceived(byte[] buffer)
+        {
+            Log.d(TAG, "onBufferReceived");
+        }
+
+        public void onEndOfSpeech()
+        {
+            Log.d(TAG, "onEndOfSpeech");
+        }
+
+        public void onError(int error)
+        {
+            if(continuousListening)
             {
-                case ERROR_AUDIO:
-                    message = "Audio recording error";
-                    break;
-                case ERROR_CLIENT:
-                    message = "Client side error";
-                    break;
-                case ERROR_INSUFFICIENT_PERMISSIONS:
-                    message = "Insufficient permissions";
-                    break;
-                case ERROR_NETWORK:
-                    message = "Network error";
-                    break;
-                case ERROR_NETWORK_TIMEOUT:
-                    message = "Network timeout";
-                    break;
-                case SpeechRecognizer.ERROR_NO_MATCH:
-                    message = "No match";
-                    break;
-                case ERROR_RECOGNIZER_BUSY:
-                    message = "RecognitionService busy";
-                    break;
-                case ERROR_SERVER:
-                    message = "error from server";
-                    break;
-                case ERROR_SPEECH_TIMEOUT:
-                    message = "No speech input";
-                    break;
-                default:
-                    message = "Didn't understand, please try again.";
-                    break;
+                RestartListening();
+            }
+        }
+
+        public void onResults(Bundle results)
+        {
+            StringBuilder str = new StringBuilder();
+            resultData = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            if (resultData != null)
+            {
+                str.append(resultData.get(0));
+                for (int i = 1; i < resultData.size(); i++)
+                {
+                    str.append("~").append(resultData.get(i));
+                }
             }
 
-            SendErrorToUnity(message);
+            System.out.println("->->->->->->->->->--------------" + str.toString());
 
-            System.out.println("-22----33----44----- " + "error occurred " + message);
-
+            SendUnityResults(str.toString());
+            if(continuousListening)
+            {
+                RestartListening();
+            }
         }
-        catch (Exception e)
+
+        public void onPartialResults(Bundle partialResults)
         {
-            e.printStackTrace();
+            Log.d(TAG, "onPartialResults");
         }
-    }
 
-    @Override
-    public void onResults(Bundle results)
-    {
-        checkForCommands(results);
-    }
-
-    @Override
-    public void onPartialResults(Bundle partialResults)
-    {
-        System.out.println("-22----33----44----- " + "partial results");
-    }
-
-    @Override
-    public void onEvent(int eventType, Bundle params)
-    {
-        System.out.println("-22----33----44----- " + "onEvent");
+        public void onEvent(int eventType, Bundle params)
+        {
+            Log.d(TAG, "onEvent " + eventType);
+        }
     }
 }
